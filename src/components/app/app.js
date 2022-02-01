cddf74818719c05967bd0eb9410888c579803089
Oleksandr <kulibaba99@gmail.com>
2022-02-02 00:57:26 +0200

    random button

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,18 +3,22 @@ import './app.css'
 
 import AppHeader from "../app-header/app-header";
 import AppTitle from "../app-title";
-import Category from "../category/category";
+import CategoryItem from "../category-item";
 import Quotes from "../quotes/quotes";
 
 const App = () => {
-    return (
-        <div>
-            <AppHeader/>
-            <AppTitle/>
-            <Category/>
-            <Quotes/>
-        </div>
-    )
+const [quote, setQuote] = useState([])
+const onRandom = (quote) => {
+    setQuote(quote);
+}
+return (
+    <div>
+        <AppHeader/>
+        <AppTitle/>
+        <div className="btn-group"><CategoryItem onClick={onRandom}/></div>
+        <Quotes quote={quote}/>
+    </div>
+)
 }
 
 export default App;
